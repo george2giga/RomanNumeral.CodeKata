bdc5b7885ebd543e9a2acae00a937ea51d5cc2b4
Giorgio <george2giga@gmail.com>
2015-03-13 14:31:53 +0000

    * committing RomanNumeral code kata

---
+++ RomanNumeral.CodeKata.Core.Tests/RomanNumeralConverterTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RomanNumeral.CodeKata.Core.Tests
+{
+
+    [TestFixture]
+    public class RomanNumeralFixture
+    {
+        [TestCase("IV", 4)]
+        [TestCase("III", 3)]
+        [TestCase("IX", 9)]
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("VI", 6)]
+        [TestCase("X", 10)]
+        [TestCase("LXIV", 64)]
+        [TestCase("LXXVIII", 78)]
+        [TestCase("MMDLXIV", 2564)]
+        [TestCase("XCVIII", 98)]
+        [TestCase("XCVII", 97)]
+        [TestCase("MCMXCVI", 1996)]
+        [TestCase("MCMXCVIII", 1998)]
+        [TestCase("MMXVIII", 2018)]
+        public void Convert_Serie_Of_Romans_Correctly(string roman, int expected)
+        {
+            var romanConverter = new RomanNumeralConverter();
+            var result = romanConverter.Convert(roman);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ RomanNumeral.CodeKata.Core/RomanNumeralConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeral.CodeKata.Core
+{
+    public class RomanNumeralConverter
+    {
+        public int Convert(string romanNumeral)
+        {
+            var reversedRomans = new string(romanNumeral.Reverse().ToArray());
+            int result = 0;
+            int last = 0;
+            for (int i = 0; i < reversedRomans.Count(); i++)
+            {
+                int numericValue = RomanToNumberValue(reversedRomans[i]);
+                if (numericValue < last)
+                {
+                    result -= numericValue;
+                }
+                else
+                {
+                    result += numericValue;
+                }
+
+                last = numericValue;
+            }
+
+            return result;
+
+
+        }
+
+
+
+        public int RomanToNumberValue(char romanNumeral)
+        {
+            switch (romanNumeral)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+            }
+            return 0;
+
+        }
+    }
+}
